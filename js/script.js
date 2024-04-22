705c8d545228be024ecee12531629298f8f68cbc
Ivanov Nikolai <102044962+qsiax@users.noreply.github.com>
2024-04-23 06:59:35 +0900

    qsiax

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+const menu = document.querySelector('.menu')
+const menuBtn = document.querySelector('.header__menu-wrapper')
+
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('active')
+    menuBtn.classList.toggle('active')
+    if (menu.classList.contains('active')) {
+        menuBtn.innerHTML = '<div class="header__menu _icon-close"></div>'
+    } else {
+        menuBtn.innerHTML = '<div class="header__menu _icon-menu"></div>'
+    }
+})
+
 const items = document.querySelectorAll('.home__item')
 
 function allUnactive() {
@@ -15,7 +28,6 @@ function allActive() {
 for (let i = 0; i < items.length; i++) {
     items[i].addEventListener('mouseover', () => {
         allUnactive()
-        items[i].classList.add('active')
     })
     items[i].addEventListener('mousemove', (e) => {
         let x = e.offsetX
@@ -32,22 +44,48 @@ for (let i = 0; i < items.length; i++) {
     items[i].addEventListener('mouseout', (e) => {
         items[i].style.transform = ``;
         allActive()
-        items[i].classList.remove('active')
     })
 }
 
-const text = document.querySelectorAll(".home__item-circle-text p");
+const header = document.querySelector('.header')
 
+window.addEventListener('scroll', (e) => {
+    const windowY = window.pageYOffset
 
-function splitText() {
-    for(let u = 0; u < text.length; u++) {
-        text[u].innerHTML = text[u].innerText
-        .split("")
-        .map(
-            (char, i) => `<span style="transform:rotate(${i * 6.5}deg)">${char}</span>`
-        )
-        .join("");
+    if( windowY > 860) {
+        header.classList.add('sticky')
     }
-}
+    if( windowY < 860) {
+        header.classList.remove('sticky')
+        menu.classList.remove('active')
+        menuBtn.classList.remove('active')
+        menuBtn.innerHTML = '<div class="header__menu _icon-menu"></div>'
+    }
+})
 
-splitText()
+const hoverEl = document.querySelector('.rukovodstvo__item-hover')
+const hoverItems = document.querySelectorAll('.rukovodstvo__item')
+
+document.addEventListener('mousemove', (e) => {
+    hoverEl.style.left = (e.pageX + 20) + 'px'
+    hoverEl.style.top = e.pageY + 'px'
+})
+
+for (i = 0; i < hoverItems.length; i++) {
+    hoverItems[i].addEventListener('mouseover', (e) => {
+        hoverEl.classList.add('active')
+        if (e.currentTarget === hoverItems[0]) {
+            hoverEl.innerHTML = 'Cпециалист по организации работы "Движение Первых" МР "Хангаласский улус"'
+        } 
+        if (e.currentTarget === hoverItems[1]) {
+            hoverEl.innerHTML = 'Председатель местного отделения МР "Хангаласский улус"'
+        }
+        if (e.currentTarget === hoverItems[2]) {
+            hoverEl.innerHTML = 'Специалист по организации работы "Движение Первых" МР "Хангаласский улус"'
+        }
+    })
+    hoverItems[i].addEventListener('mouseout', (e) => {
+        hoverEl.classList.remove('active')
+        hoverEl.innerHTML = ''
+    })
+}
---
+++ js/slider.js
@@ -0,0 +1,6 @@
+const swiper = new Swiper('.news__item-swiper ', {
+    speed: 600,
+    slidesPerView: 2.5,
+    spaceBetween: 40,
+    grabCursor: true,
+  });
