1bdc71ee0dba88e5106e16ae630730c652a61c7e
Ivanov Nikolai <102044962+qsiax@users.noreply.github.com>
2024-04-25 06:58:13 +0900

    qsiax

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,49 @@
-const swiper = new Swiper('.news__item-swiper ', {
+const swiperNews = new Swiper('.news__item-swiper ', {
     speed: 600,
     slidesPerView: 2.5,
     spaceBetween: 40,
     grabCursor: true,
+    breakpoints: {
+      // // when window width is >= 320px
+      // 320: {
+      //     slidesPerView: 1,
+      //     spaceBetween: 0,
+      //     slideToClickedSlide: true,
+      // },
+      // // when window width is >= 480px
+      // 480: {
+      //     slidesPerView: 1,
+      //     spaceBetween: 0,
+      //     slideToClickedSlide: true,
+      // },
+      // // when window width is >= 640px
+      0: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+          slideToClickedSlide: true,
+      },
+      600: {
+          slidesPerView: 2.5,
+          spaceBetween: 40,
+      }
+  }
   });
+
+const swiperGallery = new Swiper('.gallery__content  ', {
+    speed: 600,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    grabCursor: true,
+    effect: "cards",
+    navigation: {
+      nextEl: ".gallery__arrow arrow-next",
+      prevEl: ".gallery__arrow arrow-prev",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+});
