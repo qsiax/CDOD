c1c7ea805d6c38cc45da5b7faef879fccb305af5
Ivanov Nikolai <102044962+qsiax@users.noreply.github.com>
2024-04-25 07:13:37 +0900

    qsiax

---
+++ js/preloader.js
@@ -0,0 +1,49 @@
+gsap.to('.overlay-logo', 1.5, {
+	delay: 0,
+	y: 0,
+	ease: Expo.easeInOut
+})
+
+gsap.to('.overlay-logo', 1.5, {
+	delay: 2,
+	y: -100,
+	ease: Expo.easeInOut
+})
+
+gsap.to('.firts', 1.5, {
+	delay: .7,
+	height: "0%",
+	ease: Expo.easeInOut
+})
+
+gsap.to('.second', 1.5, {
+	delay: .9,
+	height: "0%",
+	ease: Expo.easeInOut
+})
+
+gsap.to('.third', 1.5, {
+	delay: 1.1,
+	height: "0%",
+	ease: Expo.easeInOut
+})
+
+
+gsap.to('.four', 1.5, {
+	delay: 1.3,
+	height: "0%",
+	ease: Expo.easeInOut
+})
+
+gsap.to('.five', 1.5, {
+	delay: 1.5,
+	height: "0%",
+	ease: Expo.easeInOut
+})
+
+
+gsap.to('.six', 1.5, {
+	delay: 1.7,
+	height: "0%",
+	ease: Expo.easeInOut
+})
